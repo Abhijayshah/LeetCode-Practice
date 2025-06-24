c0c697f1a81170c00611ff60143cf6aa098f7406
Abhijay shah <115077886+Abhijayshah@users.noreply.github.com>
2025-06-25 02:41:58 +0530

    Time: 0 ms (100%), Space: 24 MB (17.73%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -1,13 +1,13 @@
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
-        int slow = 0;
+        int slow = 0; // index to place the next non-zero
         for (int fast = 0; fast < nums.size(); ++fast) {
             if (nums[fast] != 0) {
                 if (slow != fast) {
-                    swap(nums[slow], nums[fast]);
+                    swap(nums[slow], nums[fast]); // only swap if needed
                 }
-                slow++;
+                ++slow;
             }
         }
     }
