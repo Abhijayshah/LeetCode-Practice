ee3540fa0aa194268d949f3da7c12c04030fe0cf
Abhijay Kumar Shah <115077886+Abhijayshah@users.noreply.github.com>
2026-01-12 23:35:25 +0530

    Time: 0 ms (100%), Space: 9.2 MB (48.35%) - LeetHub

---
+++ 0389-find-the-difference/0389-find-the-difference.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    char findTheDifference(string s, string t) {
+
+        char result = 0;
+
+        // XOR all characters of s
+        for (char c : s) {
+            result ^= c;
+        }
+
+        // XOR all characters of t
+        for (char c : t) {
+            result ^= c;
+        }
+
+        // Remaining character is the added one
+        return result;
+    }
+};
